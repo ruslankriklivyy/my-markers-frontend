0fc9a7cd676bc7df5066f46e25eae5db73ed2949
ruslan-quartsoft <ruslan@quartsoft.com>
2022-04-29 13:17:38 +0300

    ref: config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/header';
 import MapComp from './components/map';
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
   return (
